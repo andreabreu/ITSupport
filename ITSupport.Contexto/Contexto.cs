e64f93921ec0b5fa7f13d1952b26237492ea20a0
Andre <andre.abreu@live.com>
2016-06-02 06:16:40 -0300

    SQL Scripts Updated

--- ITSupport.Contexto/Contexto.cs
+++ ITSupport.Contexto/Contexto.cs
@@ -13,7 +13,7 @@ namespace ITSupport.Lib
         {
             string srtLocal = @"Data Source =.\SQLEXPRESS; Initial Catalog = dbITSupport; Integrated Security = SSPI;";
             string strAon = "server=BRSP015884;Trusted_Connection=yes;database=dbITSupport; user id=andre_abreu_trp;password=Suporte01; connection timeout=5";
-            minhaConexao = new SqlConnection(strAon);
+            minhaConexao = new SqlConnection(srtLocal);
             minhaConexao.Open();
         }
 
---
+++ ITSupport.Contexto/OfficeWord.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITSupport.Lib
+{
+    class OfficeWord
+    {
+        //OBJECT OF MISSING "NULL VALUE"
+        Object oMissing = System.Reflection.Missing.Value;
+
+        Object oTemplatePath = System.AppDomain.CurrentDomain.BaseDirectory + "tr.dotx";
+
+
+        Application wordApp = new Application();
+        Document wordDoc = new Document();
+
+        wordDoc = wordApp.Documents.Add(ref oTemplatePath, ref oMissing, ref oMissing, ref oMissing);
+
+            foreach (Field myMergeField in wordDoc.Fields)
+            {
+
+
+                Range rngFieldCode = myMergeField.Code;
+
+        String fieldText = rngFieldCode.Text;
+
+
+
+                // ONLY GETTING THE MAILMERGE FIELDS
+
+                if (fieldText.StartsWith(" MERGEFIELD"))
+                {
+
+                    // THE TEXT COMES IN THE FORMAT OF
+
+                    // MERGEFIELD  MyFieldName  \\* MERGEFORMAT
+
+                    // THIS HAS TO BE EDITED TO GET ONLY THE FIELDNAME "MyFieldName"
+
+                    Int32 endMerge = fieldText.IndexOf("\\");
+
+        Int32 fieldNameLength = fieldText.Length - endMerge;
+
+        String fieldName = fieldText.Substring(11, endMerge - 11);
+
+        // GIVES THE FIELDNAMES AS THE USER HAD ENTERED IN .dot FILE
+
+        fieldName = fieldName.Trim();
+
+                    // **** FIELD REPLACEMENT IMPLEMENTATION GOES HERE ****//
+
+                    // THE PROGRAMMER CAN HAVE HIS OWN IMPLEMENTATIONS HERE
+
+                    if (fieldName == "Usuario")
+                    {
+                        myMergeField.Select();
+
+                        wordApp.Selection.TypeText("Vivek");
+
+                    }
+
+}
+
+            }
+            wordDoc.SaveAs("myfile.doc");
+            wordApp.Documents.Open("myFile.doc");
+            //wordApp.Application.Quit();
+    }
+}
